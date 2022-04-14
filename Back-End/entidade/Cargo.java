9179d033f886469be1323babbea53b9b547f577d
Matheus Melle Tártari <97634893+Zelphh@users.noreply.github.com>
2022-04-14 17:36:35 -0300

    Atulização das entidades
    Atualização das entidades usando Spring Boot

--- Back-End/entidade/Cargo.java
+++ Back-End/entidade/Cargo.java
@@ -5,13 +5,4 @@ public class Cargo {
 	private int cod_cargo; //--> Primary Key
 	private String nome_cargo;
 	
-	public String getNome_cargo() {
-		return nome_cargo;
-	}
-	public void setNome_cargo(String nome_cargo) {
-		if (nome_cargo == null) {
-			throw new RuntimeException("Nome do cargo não pode ser nulo*");
-		}
-		this.nome_cargo = nome_cargo;
-	}
 }
